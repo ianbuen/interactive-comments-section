538933eda164df6ebe917a9ac8e164961465635b
ianbuen <ianbuen@gmail.com>
2023-03-28 15:18:01 +0800

    fix 'click anywhere to close modal' feature

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,9 +1,15 @@
 import "../styles/Modal.sass"
 
 export const Modal = ({action}) => {
+ 
+  // enable modal closing by clicking background overlay
+  const handleClick = ({target, currentTarget}) => {
+    if (target === currentTarget)
+        action.cancel()
+  };
 
   return (
-    <div className='Overlay' onClick={() => action.cancel()}>
+    <div className='Overlay' onClick={(e) => handleClick(e)}>
         <div className='Modal'>
             <h1>Delete comment</h1>
             <p>Are you sure you want to delete this comment? This will remove the comment and can't be undone.</p>
